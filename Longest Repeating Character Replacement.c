54043ed4f46cc6c25feb51504f8c6d8c2801cb73
Shine Vattuparampil Sam <156725278+shinesvs@users.noreply.github.com>
2025-10-13 18:34:40 -0400

    Update Longest Repeating Character Replacement.c

--- Longest Repeating Character Replacement.c
+++ Longest Repeating Character Replacement.c
@@ -1,5 +1,4 @@
 int characterReplacement(char* s, int k) {
-      
     int freq[26] = {0};
     int left = 0, maxCount = 0, best = 0;
 
@@ -7,15 +6,21 @@ int characterReplacement(char* s, int k) {
         int r = s[right] - 'A';
         freq[r]++;
         if (freq[r] > maxCount) maxCount = freq[r];
-
         // If more than k chars need replacing, shrink from the left
         while ((right - left + 1) - maxCount > k) {
             freq[s[left] - 'A']--;
             left++;
-        }
+
+            maxCount = 0;
+            for ( int i = 0  ; i < 26 ; i++){
+            if ( freq[i] > maxCount)
+               maxCount = freq[i];
+        } }
 
         int windowLen = right - left + 1;
         if (windowLen > best) best = windowLen;
     }
     return best;
+
+}
 
